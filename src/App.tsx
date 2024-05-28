3be08dacdb566824b39f6cf5ed4d5b3a8dacd8d2
alirfz <seyedalirafazi80@gmail.com>
2024-05-28 18:07:05 +0330

    install taiwind and add color

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-red-300 text-3xl">Vite + React</h1>
+      <h1 className="text-primary text-5xl">
+        سلام خوبین این برنامه من برای شماهاس
+      </h1>
+      <p>سلام این یک تست است</p>
       <div className="card">
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
